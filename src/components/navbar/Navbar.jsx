c34f5a08c1dda856b77bbc0cfd70175e0dabf913
Sandip <roysandip33290@gmail.com>
2024-05-26 11:01:52 +0530

    created resposive ness and navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./navbar.scss";
+function Navbar() {
+  return (
+    <div>
+      <nav>
+        <div className="right">Right</div>
+        <div className="left">Left</div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
+
