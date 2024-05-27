a4260e1ac006701fdcb494924dce014358a94156
Sandip <roysandip33290@gmail.com>
2024-05-27 09:16:13 +0530

    adjusted nav bar according to devices and added menu icon

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,44 @@
+import { useState } from "react";
 import "./navbar.scss";
 function Navbar() {
+  const [open, setOpen] = useState(false);
   return (
     <div>
       <nav>
-        <div className="right">Right</div>
-        <div className="left">Left</div>
+        <div className="left">
+          <a href="/" className="logo">
+            <img src="./images/dmr.png" alt="logo" />
+            <span> Real Estate</span>
+          </a>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contacts</a>
+          <a href="/">Agents</a>
+        </div>
+        <div className="right">
+          <a href="/">Sign In</a>
+          <a href="/" className="register">
+            Sign Up
+          </a>
+          <div className="menuIcon">
+            <img
+              src="./images/Menu.png"
+              alt="loading"
+              onClick={()=>setOpen((prev)=>!prev)}
+            />
+          </div>
+          <div className={open ? "menu active" : "menu"}>
+            <a href="/">Home</a>
+            <a href="/">About</a>
+            <a href="/">Contacts</a>
+            <a href="/">Agents</a>
+            <a href="/">Sign in</a>
+            <a href="/">Sign up</a>
+          </div>
+        </div>
       </nav>
     </div>
-  )
+  );
 }
 
-export default Navbar
-
+export default Navbar;
